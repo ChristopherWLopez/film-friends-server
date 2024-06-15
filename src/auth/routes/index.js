0a710af64bbffe9b180e2e4fbc5c643206e6b5a0
christopher lopez <christopherwlopez89@gmail.com>
2024-06-14 18:15:34 -0700

    added error handling

--- src/auth/routes/index.js
+++ src/auth/routes/index.js
@@ -9,11 +9,17 @@ authRoutes.post('/signup', signup);
 authRoutes.post('signin', signin);
 
 async function signup(req, res, next){
-    // on successful account creation return a 201 status eith the user object in the body
-    // username and password gets sent on the req.body these will get stores in the in our model
-    const { username, password } = req.body;
 
-    await User.createWithHashed(username, password);
-    res.send(201);
-    // on any error, trigger your error handler with an appropriate error
+    try{
+
+        // on successful account creation return a 201 status eith the user object in the body
+        // username and password gets sent on the req.body these will get stores in the in our model
+        const { username, password } = req.body;
+        
+        await User.createWithHashed(username, password);
+        res.send(201);
+    }catch(e){
+        console.error(e);
+    }
+        // on any error, trigger your error handler with an appropriate error
 }
