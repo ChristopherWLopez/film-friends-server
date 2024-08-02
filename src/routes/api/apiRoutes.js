79aac77b3b9c3fa16657fa1cb01abc2941d8b5d4
christopher lopez <christopherwlopez89@gmail.com>
2024-08-01 18:23:51 -0700

    routes for api call

---
+++ src/routes/api/apiRoutes.js
@@ -0,0 +1,18 @@
+'use strict';
+
+export default async function apiCall(req, res, next) {
+    
+    const apiKey = process.env.Movie_DB_API_KEY;
+    const apiUrl = `http://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`
+
+    try{
+        const response = await fetch(apiUrl);
+        const json = await response.json();
+
+        console.log(data);
+
+    }catch(error){
+        console.error('This is the error', error);
+    }
+
+}
--- src/server.js
+++ src/server.js
@@ -12,6 +12,12 @@ server.use(userRoutes);
 server.get('/home', (req, res)=> res.send('Welcome home'));
 
 server.get('/api', (req, res)=>{
+
+    const apiKey = process.env.apiKey
+    const apiUrl = `http://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`;
+
+
+
     res.send('This is the truth');
 });
 
