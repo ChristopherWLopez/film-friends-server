adbcafa822e4495ddfa22aef86ee567a54a84211
christopher lopez <christopherwlopez89@gmail.com>
2024-09-09 18:38:51 -0700

    trying to make api call

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
-const { userRoutes } = require('./routes/user.routes'); 
+const {userRoutes} = require('./routes/user.routes'); 
 const cors = require('cors');
+// const apiCall = require('../src/routes/api/apiRoutes')
 const server = express();
+// const axios = require('axios');
+// const fetch = require('node-fetch');
 
 server.use(cors());
 server.use(express.json());
@@ -9,18 +12,38 @@ server.use(express.json());
 server.use(userRoutes);
 
 
+
+
 server.get('/home', (req, res)=> res.send('Welcome home'));
 
-server.get('/api', (req, res)=>{
+server.get('/api', async (req, res)=>{
 
     const apiKey = process.env.apiKey
     const apiUrl = `http://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`;
+
+    try{
+        const response = fetch(apiUrl);
+
+        if(!response.ok){
+            throw new Error("Failed to fetch data");
+        }
+
+        const data = await response.json();
+
+        console.log(data);
+
+        res.json(data);
+
+    }catch(e){
+        console.error(e)
+
+        res.status(500).json({ error: "failed to fetch data"});
+    }
 
 
 
     res.send('This is the truth');
 });
-
 
 
 module.exports={
